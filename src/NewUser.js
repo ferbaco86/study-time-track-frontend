2b07c1330e45bf95712ad1f5b82b8a998a62a436
Fernando Bahamondes <ferbaco@gmail.com>
2021-03-10 23:54:40 -0300

    Add NewUser, ShowUser and Login components

---
+++ src/NewUser.js
@@ -0,0 +1,14 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+
+const NewUser = () => (
+  <form>
+    <label htmlFor="username">Username</label>
+    <input id="username" type="text" />
+    <label htmlFor="password">Password</label>
+    <input id="password" type="password" />
+    <button type="button">Sign Up</button>
+  </form>
+);
+
+export default NewUser;
---
+++ src/ShowUser.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ShowUser = () => (
+  <div>
+    Here goes the user info
+  </div>
+);
+
+export default ShowUser;
